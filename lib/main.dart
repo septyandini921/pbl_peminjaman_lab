a3524f06bda8b1dd22a21c479bb40a38f17899f5
NimasSeptiandini <septyandini921@gmail.com>
2025-11-08 09:46:42 +0700

    Configuration connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+    // Pastikan binding Flutter sudah diinisialisasi sebelum Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inisialisasi Firebase dengan konfigurasi dari firebase_options.dart
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
