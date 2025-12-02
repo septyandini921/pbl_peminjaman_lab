4ed3af517dbe2e7ee475aba8a3bbc41eadebcae6
NimasSeptiandini <septyandini921@gmail.com>
2025-12-03 02:07:03 +0700

    Admin: Konfirmasi Booking

--- lib/models/booking/booking_model.dart
+++ lib/models/booking/booking_model.dart
@@ -6,7 +6,9 @@ class BookingModel {
   final String bookCode;
   final String bookNim;
   final String bookPurpose;
+  final int participantCount;
   final bool isConfirmed;
+  final bool isRejected;    // <-- TAMBAH
   final bool isPresent;
   final DocumentReference? slotRef;
   final DocumentReference? userRef;
@@ -17,36 +19,39 @@ class BookingModel {
     required this.bookCode,
     required this.bookNim,
     required this.bookPurpose,
+    required this.participantCount,
     required this.isConfirmed,
+    required this.isRejected,    // <-- TAMBAH
     required this.isPresent,
     required this.slotRef,
     required this.userRef,
   });
 
-  // FROM FIRESTORE
-  factory BookingModel.fromFirestore(
-      String id, Map<String, dynamic> data) {
+  factory BookingModel.fromFirestore(String id, Map<String, dynamic> data) {
     return BookingModel(
       id: id,
       bookBy: data['book_by'] ?? "",
       bookCode: data['book_code'] ?? "",
       bookNim: data['book_nim'] ?? "",
       bookPurpose: data['book_purpose'] ?? "",
+      participantCount: data['participant_count'] ?? 1,
       isConfirmed: data['is_confirmed'] ?? false,
+      isRejected: data['is_rejected'] ?? false,     // <-- TAMBAH
       isPresent: data['is_present'] ?? false,
-      slotRef: data['slotId'],    // sudah DocumentReference
-      userRef: data['user_id'],   // sudah DocumentReference
+      slotRef: data['slotId'],
+      userRef: data['user_id'],
     );
   }
 
-  // KE FIRESTORE
   Map<String, dynamic> toFirestore() {
     return {
       'book_by': bookBy,
       'book_code': bookCode,
       'book_nim': bookNim,
       'book_purpose': bookPurpose,
+      'participant_count': participantCount,
       'is_confirmed': isConfirmed,
+      'is_rejected': isRejected,    // <-- TAMBAH
       'is_present': isPresent,
       'slotId': slotRef,
       'user_id': userRef,
